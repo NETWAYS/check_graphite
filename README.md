fe719bedc9e02e250d64ca54aadbd7631822e812
Jean-Marcel Flach <jflach@ws-jflach.int.netways.de>
2014-08-07 14:37:03 +0200

    Update documentation with examples Add useful error messages Make htaccess work with envs

--- README.md
+++ README.md
@@ -1,43 +1,58 @@
 check_grapite.py
 ================
-An icinga2 plugin to check on graphite graphs
+  An icinga2 plugin to check on graphite graphs
 
 ###Installation Instructions:
--Run it
+  Without icinga:  
+    -Just run it  
+
+  With icinga:  
+  **1.** Put the Script into your Pluginfolder designated by your constants.conf  
+  **2.** Create a CheckCommand object for check_Graphite.py, remember including envs for authentication.  
+  &nbsp;&nbsp;&nbsp;&nbsp;An example config is included and make sure that location is found by your icinga2.conf.  
+  **3.** Create a Service object with all the arguments you in your hostfolder.    
+  &nbsp;&nbsp;&nbsp;&nbsp;An example config is included.  
+  **4.** Done, now start icinga2.  
 
 ###Usage Instructions:  
-  -g [graph name]      Name of the graph as given by Graphite
+  -g [graph name]  
+  Name of the graph as given by Graphite
 
-  -H [URL]             URL to the page Graphite is running on,
-                       in the form of "http://url.top/"
-                       Default is "http://localhost:80/"
+  -H [URL]  
+  URL to the page Graphite is running on, in the form of "http://url.top/"  
+  Default is "http://localhost:80/"
+                      
+  -w [[u]Wthreshold]  
+  Define warning threshold, use 'u' to warn if value goes below threshold
 
-  -w [[u]Wthreshold]  Define warning threshold, only int or float
-                       use 'u' to warn if value goes below threshold
-
-  -c [[u]Cthreshold]   Define critical threshold, only int or float
-                       use 'u' to warn if value goes below thrshold
+  -c [[u]Cthreshold]  
+  Define critical threshold, use 'u' to warn if value goes below thrshold
   
-  -t [time frame]      Get data for the last X [d]ays/[h]ours/[m]inutes
-                       Default is 24 hours
+  -t [time frame]  
+  Get data for the last X [d]ays/[h]ours/[m]inutes.  
+  Default is 24 hours.
   
-  -m [mode]            Declare mode
-                       0: Default mode
-                       1: Needs -T [threshold] option, counts the time the
-                          graph is over your threshold. Can be combined
-                          with all other options
+  -m [mode]  
+  Declare mode.  
+  0: Default mode  
+  1: Needs -T [threshold] option, counts the time the graph is over your threshold.  
+  Can be combined with all other options  
    
-  -h                   Print usage
+  -h  
+  Print usage
 
-  --help               Display this site
+  --help  
+  Display verbose help
 
-  Example usage:
-  ./check_graphite -g carbon.agents.cpuUsage -H http://example.com/ -w 85.4 -c u0 -t 3d
-  Poll the graph carbon.agents.cpuUsage on example.com for the last three days, 
+  Example usage:  
+  `./check_graphite -g carbon.agents.cpuUsage -H http://example.com/ -w 85.4 -c u0 -t 3d`  
+  Poll the graph carbon.agents.cpuUsage on example.com for the last three days,  
   warning if it is over 85.4 and sending a critical if it is below 0.
 
 ###Output
-The output will be formatted the following way:
-mode dependant|most recent value;warning threshold;critical threshold;
-max value;min value;average value;sum of all values;time frame
+The output will be formatted the following way:  
+Mode 0:  
+Status|most recent value;wThreshold;cThreshold;max;min;avg;sum;time  
+Mode 1:  
+Status|count over threshold;percentile over threshold;wThreshold;cThreshold;max;min;avg;sum;time  
 
--- check_graphite.py
+++ check_graphite.py
@@ -25,6 +25,7 @@ import urllib2
 import getopt
 import sys
 import getpass
+import os
 from decimal import Decimal
 
 def main():
@@ -32,57 +33,45 @@ def main():
   cars = {'w':None, 'c':None, 't':'24h', 'H':'http://localhost:80/'}
   #parse options
   try:
-    opts, args = getopt.getopt(sys.argv[1:], "g:w:c:H:hm:t:T:", ["help"])
+    opts, args = getopt.getopt(sys.argv[1:], 'g:w:c:H:hm:t:T:', ['help'])
   except getopt.GetoptError, e:
     print e
     usage()
-    return 3
+    sys.exit(3)
   for o, a in opts:
-    if o == "-h":
+    if o == '-h':
       usage()
-      return 3
-    if o == "--help":
+      sys.exit(3)
+    if o == '--help':
       showVerboseHelp()
-      return 3
+      sys.exit(3)
     cars[o[1]] = a
 
   if 'm' in cars:
     if cars['m'] == '1':
       if not 'T' in cars:
-        print 'Mode 1 requires argument -T'
-        return 3
+        die('Mode 1 requires argument -T')
   else:
     cars['m'] = '0'
 
   if not 'g' in cars:
     print "Missing argument '-g'"
     usage()
-    return 3
+    sys.exit(3)
 
   data = getGraph(cars['g'], cars['H'], cars['t'])
-  if data[2] == 1:
-    print "Connection error"
-    return 3
-  elif data[2] == 2:
-    print "Faulty data"
-    return 3
-  elif data[2] == 3:
-    print 'Unknown time format'
-    return 3
 
   if cars['m'] == '0':
     result = handleThreshold(data[0], cars['w'], cars['c'])
 
     if result == 'ERROR':
-      print "Invalid thresholds"
-      return 3
+      die('Invalid thresholds')
     output = result+'|time='+str(data[1])+';value='+str(data[0])
 
   elif cars['m'] == '1':
     result = handleOverThreshold(data[2], cars['c'], cars['w'], cars['T'])
     if result[0] == 'ERROR':
-      print "Invalid thresholds"
-      return 3
+      die('Invalid thresholds')
     output = result[0]+'|count='+str(result[1])+';perc='+str(result[2])
 
   mmas = getMaxMinAvgSum(data[2])
@@ -90,24 +79,29 @@ def main():
     output += ';w='+cars['w']
   if cars['c'] != None:
     output += ';c='+cars['c']
+
   output += ';max='+str(mmas[0])+';min='+str(mmas[1])+';avg='+str(mmas[2])+';sum='+str(mmas[3])+';from='+cars['t']
+  #";max={mmas[0]!s};min={mmas[1]!s};avg={mmas[2]!s};sum={mmas[3]};from={cars['t']}"
+  #2.4: no proper formatting
 
   print output
 
   if result == 'CRITICAL':
-    return 2
+    sys.exit(2)
   if result == 'WARNING':
-    return 1
+    sys.exit(1)
   else:
-    return 0
+    sys.exit(0)
 
-#get latest changed data, return (x,y,z,0) on success
+
+#get latest changed data, return (x,y,z)
 def getGraph(name, url, time):
   #graphite wants full names
   try:
     int(time[:-1])
   except ValueError:
-    return (0, 0, 3)
+    die('Unknown time format')
+
   #no negatives please
   if time[0] == '-':
     time = time[1:]
@@ -118,33 +112,40 @@ def getGraph(name, url, time):
   elif time[-1] == 'm':
     time += 'inutes'
   else:
-    return (0, 0, 3)
+    die('Unknown time format')
 
-  url = url+'render?target='+name+'&format=raw&from=-'+time
+  url = '%srender?target=%s&format=raw&from=-%s' %(url, name, time)
   try:
     r = urllib2.urlopen(url)
   except urllib2.HTTPError, e: #2.4 can't into as
-    #I have no idea what I'm doing
     if e.code != 401:
-      return (0, 0, 1)
-    print 'The server asks for authentication'
-    user = raw_input('Username: ')
-    passwd = getpass.getpass('Password: ')
-    passMgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-    passMgr.add_password(None, url, user, passwd)
-    authhandler = urllib2.HTTPBasicAuthHandler(passMgr)
-    opener = urllib2.build_opener(authhandler)
-    urllib2.install_opener(opener)
-    r = urllib2.urlopen(url)
-  except:
-    return (0, 0, 1)
+      die(e)
+    try:
+      death = None #without this the exception would cath the sys.exit() exception and die again printing the exit status
+      user = os.environ.get('GRAPHITE_ACCESS_USER')
+      passwd = os.environ.get('GRAPHITE_ACCESS_PASS')
+      if user or passwd == None:
+        death = ('Server requires authentication, set your env GRAPHITE_ACCESS_USER and GRAPHITE_ACCESS_PASS accordingly')
+      passMgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+      passMgr.add_password(None, url, user, passwd)
+      authhandler = urllib2.HTTPBasicAuthHandler(passMgr)
+      opener = urllib2.build_opener(authhandler)
+      urllib2.install_opener(opener)
+      r = urllib2.urlopen(url)
+    except Exception, e:
+      if death == None:
+        die(e)
+      else:
+        die(death)
+  except Exception, e:
+    die(e)
 
   text = r.read()
   #Find latest entry
   #entry = (data,time,MaxMinAvgSum,status)
   entry = ()
   if text == None:
-    return (0, 0, 1)
+    die('Data returned by Graphite not valid')
 
   text = text.split(',')
   try:
@@ -154,7 +155,7 @@ def getGraph(name, url, time):
     text[3] = t[-1] #first entry has a '|'
     text[-1] = text[-1][:-1] #last entry has a newline
   except ValueError:
-    return (0, 0, 2)
+    die('Data returned by Graphite not valid')
 
   #traverse, collect values and keep latest entry
   vals = []
@@ -167,10 +168,11 @@ def getGraph(name, url, time):
     ctime += step
 
   if entry != ():
-    return (entry[0], entry[1], vals, 0)
+    return (entry[0], entry[1], vals)
 
   #if no entry is found return 0 as value und time of latest actualisiation
-  return (0, ctime, (0, 0, 0, 0), 0)
+  return (0, ctime, (0, 0, 0, 0))
+
 
 def handleThreshold(data, warn, crit):
   #test on critical first, in case critical <= warning
@@ -208,6 +210,7 @@ def handleThreshold(data, warn, crit):
         return 'ERROR'
 
   return 'OK'
+
 
 def handleOverThreshold(data, crit, warn, threshold):
   res = 'OK'
@@ -260,14 +263,20 @@ def handleOverThreshold(data, crit, warn, threshold):
     res = 'ERROR'
   return (res, count, perc)
 
+
 def getMaxMinAvgSum(data):
   #(max,min,avg,sum)
   a = [Decimal(i) for i in data]
   return (max(a), min(a), sum(a)/len(a), sum(a))
 
 
+def die(msg):
+  print msg
+  sys.exit(3)
+
 def usage():
   print 'Usage: \n'+sys.argv[0] + ' -g [Graph] -H [url] [-w [u][Wthreshold]] [-c [u][Cthreshold]] [-t [time frame]] [-h, --help]'
+
 
 def showVerboseHelp():
   print '  '+sys.argv[0]+' - Help\n'
@@ -278,7 +287,10 @@ def showVerboseHelp():
                        in the form of "http://url.top/"
                        Default is "http://localhost:80/"
 
-  -w [[u]Wthreshold]  Define warning threshold, only int or float
+                       Uses the enviroment variables GRAPHITE_ACCESS_USER 
+                       and GRAPHITE_ACCESS_PASS for authorization
+
+  -w [[u]Wthreshold]   Define warning threshold, only int or float
                        use 'u' to warn if value goes below threshold
 
   -c [[u]Cthreshold]   Define critical threshold, only int or float
@@ -303,5 +315,5 @@ def showVerboseHelp():
   warning if it is over 85.4 and sending a critical if it is below 0.
   '''
 
-if __name__ == "__main__":
-  sys.exit(main())
+if __name__ == '__main__':
+  main()
